e828e173d075332fe60fc5d6c6434c47c790eb45
Vinicius4444u <147526056+Vinicius4444u@users.noreply.github.com>
2024-11-12 10:38:53 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,38 +1,38 @@
 criaCartao(
     'Anime Solo Leveling',
-    'Qual o nome do caçador mais forte dos Eua em Solo Leveling?',
-    'Thomas Andre'
+    'Qual o nome do caçador mais forte dos Eua em Solo Leveling ?',
+    'Thomas Andre.'
 
 )
 
 criaCartao(
     'Anime HXH',
-    'Qual o nome do Rei das formigas quimeras?',
-    'Meruem'
+    'Qual o nome do Rei das formigas quimeras ?',
+    'Meruem.'
 )
 
 criaCartao(
     'Anime HXH',
-    'Qual o nome do homem que ensinou nen para Gon e Killua?',
-    'Wing'
+    'Qual o nome do homem que ensinou nen para Gon e Killua ?',
+    'Wing.'
 
 )
 
 criaCartao(
     'Anime HxH',
-    'Qual o nome do cão de guarda da família Zoldick?',
-    'Mike'
+    'Qual o nome do cão de guarda da família Zoldick ?',
+    'Mike.'
 
 )
 
 criaCartao(
     'Anime HxH',
-    'Ging pai de Gon é um hunter de quantas estrelas?',
-    'Ele é um hunter de duas estrelas'
+    'Ging pai de Gon é um hunter de quantas estrelas ?',
+    'Ele é um hunter de duas estrelas.'
 )
 
 criaCartao(
     'Anime Fullmetal Alchemist',
-    'Qual é o nome do alquimista mais poderoso?',
-    'Roy Mustang'
+    'Qual é o nome do alquimista mais poderoso ?',
+    'Roy Mustang.'
 )
